4d9d439669efb79d2dde79699e93a1f8fd01e5f2
samitin <samitin.e@gmail.com>
2021-09-14 01:22:48 +1100

    Задача для самостоятельного изучения: создайте два разных приложения и передавайте между ними данные или создайте привязанный сервис, как это описывается в факультативе.Приложение получающее сообщение

---
+++ app/src/main/java/ru/samitin/lesson6resiver/MainActivity.java
@@ -0,0 +1,35 @@
+package ru.samitin.lesson6resiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initNotificationChannel();
+    }
+
+    // инициализация канала нотификаций
+    private void initNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel("2", "name", importance);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
